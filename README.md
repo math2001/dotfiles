29d2dc6f68ac610e298f8da9930f798947c23b7c
Mathieu PATUREL <australie.p@gmail.com>
2019-11-12 16:04:45 +1100

    Ubuntu update

--- README.md
+++ README.md
@@ -15,5 +15,5 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/fish ~/.config/fish
+ln -s ~/dotfiles/fish/ ~/.config/
 ```
--- fish/config.fish
+++ fish/config.fish
@@ -7,24 +7,24 @@ set fish_color_autosuggestion brblack
 
 set -gx PATH ~/.local/bin/ ~/go/bin ~/.gem/ruby/2.5.0/bin $PATH
 
-alias curl="curl -L"
-alias ncurl="curl --proxy 'proxy.det.nsw.edu.au:8080' --proxy-ntlm --proxy-user 'mathieu.paturel'"
-alias ls="ls -NF --color=auto --group-directories-first"
-alias gittips='gittips ~/.cache/git-tips.json'
+alias curl "curl -L"
+alias ncurl "curl --proxy 'proxy.det.nsw.edu.au:8080' --proxy-ntlm --proxy-user 'mathieu.paturel'"
+alias ls "ls -NF --color=auto --group-directories-first"
+alias gittips 'gittips ~/.cache/git-tips.json'
 
-abbr -a password='date +%s | sha256sum | base64 | head -c 32 | xclip -selection clipboard'
+abbr -a password 'date +%s | sha256sum | base64 | head -c 32 | xclip -selection clipboard'
 
-alias fullbat="upower -i /org/freedesktop/UPower/devices/battery_BAT1"
-alias bat="fullbat | command grep 'percentage\|state\|time to'"
-alias notepad='vim -u ~/.vimrc.min'
-alias l='history | head -n 1'
-alias types='cat ~/.local/angular-commit-types'
+alias fullbat "upower -i /org/freedesktop/UPower/devices/battery_BAT1"
+alias bat "fullbat | command grep 'percentage\|state\|time to'"
+alias notepad 'vim -u ~/.vimrc.min'
+alias l 'history | head -n 1'
+alias types 'cat ~/.local/angular-commit-types'
 
-abbr -a ip='ip -c'
-abbr -a path='echo $PATH | tr " " "\n" '
+abbr -a ip 'ip -c'
+abbr -a path 'echo $PATH | tr " " "\n" '
 
 # git abbreviations
-abbr -a gs='git status --short'
+abbr -a gs 'git status --short'
 abbr -a gc 'git commit'
 abbr -a gca 'git commit -a'
 abbr -a gl 'git log --oneline --decorate --graph --all'
